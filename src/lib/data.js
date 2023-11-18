5b07230eb259fe776fe3a0cc612c5ff30bc912d8
Ashkie <ashkiecharles@gmail.com>
2023-11-17 21:19:07 -0700

    added the right list

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,24 @@
 let foodList = [
-  { name: 'Bacon', description: 'Description', href: '#', pic:'../images/bacon.png'
+  { name: 'Bacon Burger', description: 'Description', href: '#', pic:'../images/bacon.png'
   , attribute: "Bacon" },
+  { name: 'Bruger', description: 'Description', href: '#', pic:'../images/burger.png'
+  , attribute: "Bruger" },
+  { name: 'Chicken Burger', description: 'Description', href: '#', pic:'../images/chicken-burger.png'
+  , attribute: "Chicken-Burger" },
+  { name: 'Chicken Strips', description: 'Description', href: '#', pic:'../images/chicken-strip.png'
+  , attribute: "Chicken-Strips" },
+  { name: 'Salmon', description: 'Description', href: '#', pic:'../images/salmon.png'
+  , attribute: "Salmon" },
+  { name: 'Shrimp Taco', description: 'Description', href: '#', pic:'../images/shrimp-taco.png'
+  , attribute: "Shrimp-Taco" },
+  { name: 'Steak', description: 'Description', href: '#', pic:'../images/steak.png'
+  , attribute: "Steak" },
+  { name: 'Taco', description: 'Description', href: '#', pic:'../images/taco.png'
+  , attribute: "Taco" },
+  { name: 'Wings', description: 'Description', href: '#', pic:'../images/wings.png'
+  , attribute: "Wings" },
+  
+  
 ]
 
   ;
