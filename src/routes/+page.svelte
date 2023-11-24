614e8d2b5688daad0ce3763fb5a7121e3ec1f88d
edwardm8 <edwardm825@gmail.com>
2023-11-24 13:38:23 -0700

    test image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@ const diffDate = oldDate.diff(now, 'day')
 
 <style>
   .hero{
-    background-image: url('hero.png');
+    background-image: url('$lib/assets/hero.png');
     height: 100vh;
     background-repeat: no-repeat;
     background-size: cover;
